61e450455bf594d0fa1adbfdbace68e293d8c152
aarushinternational294 <aarushinternationaleducation@gmail.com>
2024-04-16 11:13:20 +0545

    removed useless by git ignore

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"x+vXcppP1DHicVz5dzZe+lUFw9ausPNogk4FCOEJQSM=\"}"
--- .next/static/chunks/app/HeroTwo/page-73a1d2c96b40667b.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[606],{159:function(n,e,u){Promise.resolve().then(u.bind(u,7228))}},function(n){n.O(0,[699,387,676,291,815,228,971,69,744],function(){return n(n.s=159)}),_N_E=n.O()}]);
